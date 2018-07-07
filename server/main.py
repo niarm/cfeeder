4a89d8652acd62dfaffca50b8ccb5ba9d31dd23c
Niklas Armbruster <niklas.armbruster@deutschebahn.com>
2018-07-07 13:26:54 +0200

    cont

--- server/main.py
+++ server/main.py
@@ -2,11 +2,19 @@ from physicalmachine.servocontroller import ServoController
 
 class CatfeederMainApplication():
     def __init__(self):
-        self.run()
+        self.setup()
+        while True:
+            self.run()
 
-    def run(self):
+    def setup(self):
         print("Starting Catfeeder Main Application");
         self.servoLeft = ServoController(pin=17)
+
+    def run(self):
+        self.checkSensors()
+
+    def checkSensors(self):
+        print("checking Sensors for new Input")
 
 if __name__ == "__main__":
     app = CatfeederMainApplication()
