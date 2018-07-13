5e17d7cf554807984c72c4d58411932a2e2ab17d
Niklas Armbruster <niklas.armbruster@deutschebahn.com>
2018-07-13 14:30:24 +0200

    move stepper first test cont

--- server/physicalmachine/steppercontroller.py
+++ server/physicalmachine/steppercontroller.py
@@ -1,5 +1,5 @@
 from time import sleep
-import RPi.GPIO as GPIO
+from .Stepper import stepper
 
 class StepperController:
     def __init__(self, stepper_id, pin_enable, pin_step, pin_dir, steps_per_revolution=200):
@@ -7,27 +7,14 @@ class StepperController:
         self.pin_enable = pin_enable
         self.pin_step = pin_step
         self.pin_dir = pin_dir
-        self.steps_per_revolution = steps_per_revolution
-        self.delay = .02
         
         self.setup()
 
     def setup(self):
         print("setting up StepperController with GPIO-CONFIG: (EN = "+ str(self.pin_enable)+", STEP = "+ str(self.pin_step)+", DIR = "+ str(self.pin_dir) + ")")
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.pin_enable, GPIO.OUT)
-        GPIO.setup(self.pin_step, GPIO.OUT)
-        GPIO.setup(self.pin_dir, GPIO.OUT)
-
-        GPIO.output(self.pin_enable, GPIO.LOW) #high is stop
+        self.stepperMotor = stepper([self.pin_step, self.pin_dir, self.pin_enable])
         print("... setup done")
 
     def rotateSteps(self, num_steps, direction=1):
         print("Moving Stepper"+ str(self.id) +" for num_steps: "+ str(num_steps) + " in directon: "+str(direction)+"(CW=1, CCW=0)")
-        GPIO.output(self.pin_dir, direction)
-        
-        for x in range(num_steps):
-            GPIO.output(self.pin_step, GPIO.HIGH)
-            sleep(self.delay)
-            GPIO.output(self.pin_step, GPIO.LOW)
-            sleep(self.delay)
+        self.stepperMotor.step(num_steps, "right")
